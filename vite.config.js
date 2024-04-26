cbfaa87410d11d92722c6311b282c931cebae0ab
ruuuu <rufinka_91@mail.ru>
2024-04-26 20:09:02 +0300

    optimize images

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 
 export default defineConfig({
@@ -7,4 +8,49 @@ export default defineConfig({
   build: {
     outDir: '../dist',
  },
+ plugins: [
+    ViteImageOptimizer({
+        svg: {
+            multipass: true,
+            plugins: [
+              {
+                name: 'preset-default',
+                params: {
+                  overrides: {
+                    cleanupNumericValues: false,
+                    removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+                  },
+                  cleanupIDs: {
+                    minify: false,
+                    remove: false,
+                  },
+                  convertPathData: false,
+                },
+              },
+              'sortAttrs',
+              {
+                name: 'addAttributesToSVGElement',
+                params: {
+                  attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+                },
+              },
+            ],
+          },
+          png: {
+            quality: 80,
+          },
+          jpeg: {
+            quality: 80,
+          },
+          jpg: {
+            quality: 80,
+          },
+          webp: {
+            quality: 80,
+          },
+          avif: {
+            quality: 70,
+          },
+    }),
+  ],
 });
